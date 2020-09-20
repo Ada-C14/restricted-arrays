c6a9bf572e73bffa39a4e884315e25ea144badeb
Lina Do <l.do5147@gmail.com>
2020-09-20 01:55:29 -0700

    added a length method

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -6,10 +6,18 @@ require_relative 'restricted_array.rb'
 
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def length(array)
-  raise NotImplementedError
+  count = 0
+  index = 0
+
+  until array[index] == nil
+    count += 1
+    index += 1
+  end
+
+  return count
 end
 
 # Prints each integer values in the array
