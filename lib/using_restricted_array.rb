e5647570129677c51a577f026976f8de21cf1124
KayKay-git <kayalew@yahoo.com>
2020-09-21 15:00:06 -0400

    Complete up to find_smallest method - test for largest and smallest not passing

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -6,17 +6,26 @@ require_relative 'restricted_array.rb'
 
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
-# Time complexity: ?
+# Time complexity: 0(n) iterating thrugh until we find nil
 # Space complexity: ?
 def length(array)
-  raise NotImplementedError
+  # raise NotImplementedError
+  i = 0
+  until array[i] == nil
+    i += 1
+  end
+  return i
 end
 
 # Prints each integer values in the array
 # Time complexity: ?
 # Space complexity: ?
 def print_array(array)
-  raise NotImplementedError
+  i = 0
+  until array[i] == nil
+    puts array[i]
+    i += 1
+  end
 end
 
 # For an unsorted array, searches for 'value_to_find'.
@@ -24,7 +33,13 @@ end
 # Time complexity: ?
 # Space complexity: ?
 def search(array, length, value_to_find)
-  raise NotImplementedError
+  # raise NotImplementedError
+  length.times do |i|
+    if value_to_find == array[i]
+      return true
+    end
+  end
+  return false
 end
 
 # Finds and returns the largest integer value the array
@@ -32,7 +47,14 @@ end
 # Time complexity: ?
 # Space complexity: ?
 def find_largest(array, length)
-  raise NotImplementedError
+  #raise NotImplementedError
+  highest = array[0]
+  length.times do |i|
+    if array[i] > highest
+      array[i] == highest
+    end
+  end
+  return highest
 end
 
 # Finds and returns the smallest integer value in the array
@@ -40,14 +62,22 @@ end
 # Time complexity: ?
 # Space complexity: ?
 def find_smallest(array, length)
-  raise NotImplementedError
+  #raise NotImplementedError
+  lowest = array[0]
+  length.times do |i|
+    if array[i] < lowest
+      array[i] == lowest
+    end
+  end
+  return lowest
 end
 
 # Reverses the values in the integer array in place
 # Time complexity: ?
 # Space complexity: ?
 def reverse(array, length)
-  raise NotImplementedError
+  #raise NotImplementedError
+
 end
 
 # For an array sorted in ascending order, searches for 'value_to_find'.
